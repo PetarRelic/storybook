66776cd98721cb87b29c24a7fb3cd1bce0a8146b
PetarRelic <petar.relic@gmail.com>
2018-05-19 15:55:22 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It works!');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
